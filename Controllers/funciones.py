7fd87e0dfedd55b39477f1ca64d894a7e317b51d
juan diego <juandiego@pop-os.bbrouter>
2025-08-13 23:32:31 -0500

    se crearon clases vacias

---
+++ Controllers/funciones.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import messagebox
+class Funciones ():
+    def Play(self):
+        pass
+    
+    def __init__(self):
+        pass
---
+++ Controllers/seguridad.py
@@ -0,0 +1,8 @@
+from Controllers.funciones import Funciones
+from Controllers.usuario import Usuario
+from Views.interfaz import Interfaz
+from Models.CONEXIONBD import ConexionDB
+
+class Seguridad ():
+    def __init__(self):
+        pass
